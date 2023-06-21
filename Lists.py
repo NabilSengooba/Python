c01cef542c23072fd6f5ecf7c6f38269db8fa4dd
Nabil Sengooba <107504279+NabilSengooba@users.noreply.github.com>
2023-06-21 12:39:27 +0300

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,12 @@
+names = ['nabil', 'sengooba', 'hassan', 'rayan']
+
+#adding lsits together
+total_names = ["jasper","jerry"] + names
+
+print(total_names)
+
+#using append method
+
+total_names.append("nyanzi")
+
+print(total_names)
